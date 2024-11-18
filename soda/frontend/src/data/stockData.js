54e9e9dc3e3f31c8b8af91622f516baa7139f4fc
Dane <dan.maldonado.2132@lacetania.cat>
2024-11-18 16:06:16 +0100

    asd

--- soda/frontend/src/data/stockData.js
+++
@@ -1,9 +0,0 @@
-// Datos de acciones de ejemplo
-export const stockData = [
-    { time: '1D', price: 158 },
-    { time: '2D', price: 165 },
-    { time: '3D', price: 162 },
-    { time: '4D', price: 172 },
-    { time: '5D', price: 168 },
-  ];
-  
